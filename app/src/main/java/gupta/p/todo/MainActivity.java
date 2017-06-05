9f25ed72ce53ecf746080a0b164756a027fbf848
Prakriti Gupta <prakriti.gupta10@gmail.com>
2017-06-05 17:30:33 -0400

    Date Time and Delete to a reminder added

--- app/src/main/java/gupta/p/todo/MainActivity.java
+++ app/src/main/java/gupta/p/todo/MainActivity.java
@@ -1,5 +1,9 @@
 package gupta.p.todo;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,61 +11,142 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonDate,buttonTime;
+    Button buttonDate, buttonTime;
     EditText editTextAbout;
     ImageButton imageButtonAdd;
     ListView listView;
-    String value;
+    String value,time,dates,remind;
     ArrayAdapter arrayAdapter;
+    ArrayList<String> arrayListRem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView= (ListView) findViewById(R.id.listView);
-        imageButtonAdd= (ImageButton) findViewById(R.id.imageButtonAdd);
+        listView = (ListView) findViewById(R.id.listView);
+        imageButtonAdd = (ImageButton) findViewById(R.id.imageButtonAdd);
         editTextAbout = (EditText) findViewById(R.id.editTextAbout);
         buttonDate = (Button) findViewById(R.id.buttonDate);
+        buttonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCalendar();
+            }
+        });
         buttonTime = (Button) findViewById(R.id.buttonTime);
+        buttonTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showClocK();
+            }
+        });
 
-        final ArrayList<String> arrayListRem = new ArrayList<>();
-
-        arrayAdapter= new ArrayAdapter(this,R.layout.list_view,arrayListRem);
+        arrayListRem = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter(this, R.layout.list_view, arrayListRem);
         listView.setAdapter(arrayAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                arrayListRem.remove(position);
-                arrayAdapter.notifyDataSetChanged();
-
+                showListDialog(position);
             }
         });
 
         imageButtonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                value=editTextAbout.getText().toString();
-                if(value.equals("")) {
-                    Toast.makeText(MainActivity.this, "Enter Value First", Toast.LENGTH_SHORT).show();
+                remind = editTextAbout.getText().toString();
+                if(remind.equals("")) {
+                    Toast.makeText(MainActivity.this, "Add Reminder Name", Toast.LENGTH_SHORT).show();
                 }
                 else {
+                    value = remind + "\n" + dates + "\n" + time;
                     arrayListRem.add(value);
                     arrayAdapter.notifyDataSetChanged();
                     editTextAbout.setText("");
+                    buttonTime.setText("Select Time");
+                    buttonDate.setText("Select Date");
                 }
             }
         });
+    }
+    private void showCalendar() {
+        Calendar calendar= Calendar.getInstance();
+        final int date=calendar.get(Calendar.DAY_OF_MONTH);
+        int month=calendar.get(Calendar.MONTH);
+        int year=calendar.get(Calendar.YEAR);
+        DatePickerDialog datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                buttonDate.setText(""+dayOfMonth+"-"+month+"-"+year);
+                dates="Date: "+dayOfMonth+"-"+month+"-"+year;
+            }
+        },year,month,date);
+        datePickerDialog.show();
+    }
+    private void showClocK() {
+        Calendar calendar= Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog=new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                buttonTime.setText(""+hourOfDay+":"+minute);
+                time="Time: "+hourOfDay+":"+minute;
+            }
+        },hour,minute,true);
+        timePickerDialog.show();
+    }
+    private void showListDialog(final int pos)
+    {
 
+        String[] arr = {"Delete","Edit"};
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Select Options");
+        builder.setItems(arr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int position) {
+                if(position==0) {
+                    showDelDialog(pos);
+                }
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+    public void showDelDialog(final int position) {
+        AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Confirmation");
+        builder.setMessage("Do you want to delete?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                arrayListRem.remove(position);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNeutralButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog deleteDialog=builder.create();
+        deleteDialog.show();
     }
 }
